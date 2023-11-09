556660275f614b0f9f8ec18a532856c6bb0cfd25
Luke Booth <luke.booth@cph.org>
2023-11-09 13:34:13 -0600

    [skip] Allowed clearing a number (5m)

--- addon/components/phone-input.js
+++ addon/components/phone-input.js
@@ -266,9 +266,7 @@ export default Component.extend({
       this._iti.setCountry(this.country);
     }
 
-    if (this.number) {
-      this._iti.setNumber(this.number);
-    }
+    this._iti.setNumber(this.number);
   },
 
   _onCountryChange() {
